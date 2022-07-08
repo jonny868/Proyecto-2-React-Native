64b0328c281aa8ba8e8f8f68ae1f378da988de2a
jonny <jonny868221@gmail.com>
2022-07-08 09:19:06 -0400

    backend thingy

--- backend/src/models/Tweet.model.js
+++
@@ -1,9 +0,0 @@
-const { default: mongoose, Schema, model, mongo } = require("mongoose");
-
-const tweetSchema = new Schema({
-    text: String
-})
-
-const Tweet = mongoose.model('Tweet', tweetSchema);
-
-module.exports = Tweet
